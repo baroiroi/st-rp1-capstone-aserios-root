d63decf1f9b307bd97ba59ff76d4cfdeb37eb6d3
baroiroi <johnroy0414@gmail.com>
2023-08-23 17:28:17 +0800

    new update

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,10 +3,12 @@ import Login from './authentication/Login'
 import UserBooking from './components/UserBooking'
 import { Modal } from './components/Modal'
 import { Routes, Route } from 'react-router-dom'
+import NavBar from './common/NavBar'
 
 const App = () => {
     return (
         <div>
+            <NavBar />
             <Routes>
                 <Route path='/' element={<Login />} />
                 <Route path='/Modal' element={<Modal />} />
