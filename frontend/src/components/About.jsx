a04e67ee4ba49ab30285df88662e9f04a550f048
baroiroi <johnroy0414@gmail.com>
2023-10-10 03:14:59 +0800

    update About page

--- frontend/src/components/About.jsx
+++ frontend/src/components/About.jsx
@@ -1,6 +1,9 @@
 import pic1 from '../assets/pic1.jpg'
 import pic2 from '../assets/pic2.jpg'
 import pic3 from '../assets/pic3.jpg'
+import pic4 from '../assets/pic4.jpg'
+import pic5 from '../assets/pic5.jpg'
+import pic6 from '../assets/pic6.jpg'
 import { AboutUs, VisionMission } from '../utils/data';
 
 const About = () => {
@@ -11,16 +14,16 @@ const About = () => {
                 return(
                   <li key={index}>
                     <div className='flex justify-center py-5'><h1>{About.name}</h1></div>
-                    <div className='px-5'>{About.description}</div>
+                    <div className='px-5 pb-5'>{About.description}</div>
                   </li>
                 ) 
               })}
 
           </ul>
           <div className='flex flex-wrap justify-between'>
-            <img src={pic1} alt="sample pic1" className='px-5 w-96 h-72' />
-            <img src={pic2} alt="sample pic2" className='px-5 w-96 h-72' />
-            <img src={pic3} alt="sample pic3" className='px-5 w-96 h-72' />
+            <img src={pic1} alt="sample pic 1" className='px-5 w-96 h-72' />
+            <img src={pic2} alt="sample pic 2" className='px-5 w-96 h-72' />
+            <img src={pic3} alt="sample pic 3" className='px-5 w-96 h-72' />
           </div>
           <span className='flex justify-center py-5 pl-5'><h1>Our Vision and Mission</h1></span>
           <ul>
@@ -33,6 +36,11 @@ const About = () => {
                 )
           })}
           </ul>
+          <div className='flex flex-wrap justify-between'>
+            <img src={pic4} alt="sample pic 4" className='px-5 w-96 h-72' />
+            <img src={pic5} alt="sample pic 5" className='px-5 w-96 h-72' />
+            <img src={pic6} alt="sample pic 6" className='px-5 w-96 h-72' />
+          </div>
     </div>
   )
 }
