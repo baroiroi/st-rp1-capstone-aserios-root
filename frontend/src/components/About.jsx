20381a17cf15c6a48cde9e7a5692fba3b64f4496
baroiroi <johnroy0414@gmail.com>
2023-10-09 20:00:46 +0800

    update of about component and added new pix

--- frontend/src/components/About.jsx
+++ frontend/src/components/About.jsx
@@ -1,9 +1,11 @@
-import Logo from '../assets/DentalSmileLogo.jpg'
+import pic1 from '../assets/pic1.jpg'
+import pic2 from '../assets/pic2.jpg'
+import pic3 from '../assets/pic3.jpg'
 import { AboutUs, VisionMission } from '../utils/data';
 
 const About = () => {
   return (
-    <div className='h-screen bg-aqua text-lightgray'>
+    <div className='h-auto bg-aqua text-lightgray'>
           <ul>
               {AboutUs.map((About, index)=> {
                 return(
@@ -13,18 +15,24 @@ const About = () => {
                   </li>
                 ) 
               })}
+
           </ul>
-          <span className='flex justify-center py-5'><h1>Our Vision and Mission</h1></span>
+          <div className='flex flex-wrap justify-between'>
+            <img src={pic1} alt="sample pic1" className='px-5 w-96 h-72' />
+            <img src={pic2} alt="sample pic2" className='px-5 w-96 h-72' />
+            <img src={pic3} alt="sample pic3" className='px-5 w-96 h-72' />
+          </div>
+          <span className='flex justify-center py-5 pl-5'><h1>Our Vision and Mission</h1></span>
           <ul>
           {VisionMission.map((vismis, index)=> {
                 return(
                   <li key={index}>
                     <div className='flex justify-start pl-5'><h2>{vismis.name}</h2></div>
-                    <div className='px-5'>{vismis.description}</div>
+                    <div className='px-5 mb-5'>{vismis.description}</div>
                   </li>
                 )
           })}
-          </ul> 
+          </ul>
     </div>
   )
 }
